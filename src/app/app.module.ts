16a32b34c1926668cf565b289b59f56e2a99cb8e
Manan Satra <msatra@stevens.edu>
2018-08-02 20:18:37 -0400

    App component changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import {PostsCreateComponent} from './posts/posts-create/posts-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostsListComponent } from './posts/posts-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsCreateComponent,
+    HeaderComponent,
+    PostsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
