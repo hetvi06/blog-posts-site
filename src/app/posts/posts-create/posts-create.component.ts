dca9d96f3989d92c856e24325af37851ccbe3fac
Manan Satra <msatra@stevens.edu>
2018-08-06 18:50:30 -0400

    added form field and used forms instead of 2way binding. Also added basic validation

--- src/app/posts/posts-create/posts-create.component.ts
+++ src/app/posts/posts-create/posts-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core'; // EventEmitter needed to create events of our own
 import {Post} from '../post.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-posts-create',
@@ -14,10 +15,16 @@ export class PostsCreateComponent {
   @Output() postCreatedEvent=new EventEmitter<Post>(); //<Post> says that the data type of the emmitted event will be post. If we dont write
   // <Post> then it is generic type
 
-  onClickButtonSavePost() {
+  onClickButtonSavePost(postForm: NgForm) {
+    if(postForm.invalid)
+    {
+      return;
+    }
     const post:Post= {
-      title:this.enteredTitle,
-      content:this.enteredContent
+      // title:this.enteredTitle,
+      // content:this.enteredContent
+      title:postForm.value.titleInput,
+      content:postForm.value.contentInput
     };
     this.postCreatedEvent.emit(post);
   }
