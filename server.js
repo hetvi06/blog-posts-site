882710624fc2d5f676f830e99c400efb0d022f1a
Manan Satra <msatra@stevens.edu>
2018-08-07 20:30:42 -0400

    enhanced server code

--- server.js
+++ server.js
@@ -1,10 +1,53 @@
-const http=require('http');
-const app=require('./nodeBackend/app');
+const app = require("./nodeBackend/app");
+const debug = require("debug")("node-angular");
+const http = require("http");
 
-const port=process.env.PORT || 3000;
+const normalizePort = val => {
+  var port = parseInt(val, 10);
 
-app.set('port',port); // this tells express on what port we are working.
-const server=http.createServer(app);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port); // this tells express on what port we are working. the "port" is a keyword and the second port can be named anything
+
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
 //this is used for the server to actually listen to requests.
 server.listen(port); //this will make sure that the server is either running on a designated port provided by the web server provider or port 3000 if nothing is provided
